0612678914fc6707ca9472ec8294f8e4348d9a43
Wilmer <vvilmervd@gmail.com>
2018-02-21 16:50:15 -0600

    Model User
    Understanding Models

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ForoDeReunionesYsoporte.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Password { get; set; }
+        public decimal Account { get; set; }
+    }
+}
