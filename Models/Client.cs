dc8ffb8fb443a4ace7073ca371a92dd28e2d9ff9
Wilmer <vvilmervd@gmail.com>
2018-02-26 14:30:53 -0600

    class change
    asd

--- Models/Client.cs
+++ Models/Client.cs
@@ -5,10 +5,10 @@ namespace ForoDeReunionesYsoporte.Models
     public class Client
     {
         public int ID { get; set; }
+        public string Name { get; set; }
         public string IdentityCard { get; set; }
         public string WebPage { get; set; }
         public string Address { get; set; }
-        public string address { get; set; }
         public int Phone { get; set; }
         public string Sector { get; set; }
     }
--- Models/Meeting.cs
+++ Models/Meeting.cs
@@ -1,15 +1,17 @@
-/*using System;
+using System;
 
 namespace ForoDeReunionesYsoporte.Models
 {
-    public class Contact
+    public class Meeting
     {
         public int ID { get; set; }
-        public string meetingTittle  { get; set; }
-        public Datetime date { get; set; }
-        public User user { get; set; }
-        public boolean virtual { get; set; }
-        public Client client { get; set; }
+        public string MeetingTittle  { get; set; }
+        public DateTime Date { get; set; }
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+        public Boolean Virtual { get; set; }
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
     
     }
-} */
+} 
